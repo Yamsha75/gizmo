5a2de7236188ed7be2b74a5daa11b739e467f980
Yamsha <jakub.lacinski@gmail.com>
2020-08-11 08:47:58 +0200

    Add server_objects - adds sync for client-side functions: - setObjectBreakable(object Object, boolean Enabled) - setObjectMass(object Object, float mass)

---
+++ server_objects/server_objects_c.lua
@@ -0,0 +1,24 @@
+-- main functions
+local function syncSetObjectBreakable(enabled)
+    setObjectBreakable(source, enabled)
+end
+addEvent("server_objects:setObjectBreakable", true)
+addEventHandler("server_objects:setObjectBreakable", root, syncSetObjectBreakable)
+
+local function syncSetObjectMass(value)
+    setObjectMass(source, value)
+end
+addEvent("server_objects:setObjectMass", true)
+addEventHandler("server_objects:setObjectMass", root, syncSetObjectMass)
+
+-- sync functions
+local function sendReadyMessage()
+    triggerServerEvent("server_objects:requestData", resourceRoot)
+end
+
+local function receiveData(breakable, new_mass)
+    for object, enabled in pairs(breakable) do setObjectBreakable(object, enabled) end
+    for object, value in pairs(new_mass) do setObjectMass(object, value) end
+end
+addEvent("server_objects:sendData", true)
+addEventHandler("server_objects:sendData", resourceRoot, receiveData)
---
+++ server_objects/server_objects_s.lua
@@ -0,0 +1,43 @@
+-- main functions
+local breakable = {}
+local new_mass = {}
+
+function setObjectBreakable(object, enabled)
+    if getElementType(object) ~= "object" then return false end
+    if not enabled then
+        breakable[object] = nil
+    else
+        breakable[object] = true
+    end
+    triggerClientEvent("server_objects:setObjectBreakable", object, enabled)
+end
+
+function setObjectMass(object, value)
+    if getElementType(object) ~= "object" then return false end
+    new_mass[object] = value
+    triggerClientEvent("server_objects:setObjectMass", object, value)
+end
+
+-- sync functions
+local clients_synced = {}
+
+local function sendData()
+    if clients_synced[client] then return end
+    triggerClientEvent(client, "server_objects:sendData", root, breakable, new_mass)
+    clients_synced[client] = true
+end
+addEvent("server_objects:requestData", true)
+addEventHandler("server_objects:requestData", root, sendData)
+
+local function removeDataSentFlag()
+    if isElement(source) then data_sent[source] = nil end
+end
+addEventHandler("onPlayerQuit", root, removeDataSentFlag)
+
+local function forgetDestroyedObject()
+    if isElement(source) and getElementType(source) == "object" then
+        breakable[source] = nil
+        new_mass[source] = nil
+    end
+end
+addEventHandler("onElementDestroy", root, forgetDestroyedObject)
