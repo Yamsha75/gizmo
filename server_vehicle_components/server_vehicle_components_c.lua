551977f94624706797545c33cf108391fd8ddb92
Yamsha <jakub.lacinski@gmail.com>
2020-08-11 12:22:06 +0200

    Add server_vehicle_components - adds sync for client-side vehicle component functions: - set/get visibility - set/get position - set/get rotation

---
+++ server_vehicle_components/server_vehicle_components_c.lua
@@ -0,0 +1,121 @@
+-- main functions
+latent_vehicles = {}
+-- latent_vehicles[<vehicle>] = {
+--     visibles = {<component> = bool},
+--     positions = {<component> = {x, y, z, base}},
+--     rotations = {<rotation> = {rx, ry, rz, base}}
+-- }
+
+function addLatentVehicle(vehicle, data)
+    print("latent vehicle!!!")
+    latent_vehicles[vehicle] = data or {}
+    addEventHandler("onClientElementStreamIn", vehicle, syncLatentVehicles)
+end
+
+function syncVehicleComponentVisible(component, visible)
+    if isElementStreamedIn(source) then
+        setVehicleComponentVisible(source, component, visible)
+    else
+        if not latent_vehicles[source] then addLatentVehicle(source) end
+        if not latent_vehicles[source].visibles then
+            latent_vehicles[source].visibles = {}
+        end
+        latent_vehicles[source].visibles[component] = visible
+    end
+end
+addEvent("server_vehicle_components:setVehicleComponentVisible", true)
+addEventHandler(
+    "server_vehicle_components:setVehicleComponentVisible", root,
+        syncVehicleComponentVisible
+)
+
+function syncVehicleComponentPosition(component, x, y, z, base)
+    if isElementStreamedIn(source) then
+        setVehicleComponentPosition(source, component, x, y, z, base)
+    else
+        if not latent_vehicles[source] then addLatentVehicle(source) end
+        if not latent_vehicles[source].positions then
+            latent_vehicles[source].positions = {}
+        end
+        latent_vehicles[source].positions[component] = {x, y, z, base}
+    end
+end
+addEvent("server_vehicle_components:setVehicleComponentPosition", true)
+addEventHandler(
+    "server_vehicle_components:setVehicleComponentPosition", root,
+        syncVehicleComponentPosition
+)
+
+function syncSetVehicleComponentRotation(component, rx, ry, rz, base)
+    if isElementStreamedIn(source) then
+        setVehicleComponentRotation(source, component, rx, ry, rz, base)
+    else
+        if not latent_vehicles[source] then addLatentVehicle(source) end
+        if not latent_vehicles[source].rotations then
+            latent_vehicles[source].rotations = {}
+        end
+        latent_vehicles[source].rotations[component] = {rx, ry, rz, base}
+    end
+end
+addEvent("server_vehicle_components:setVehicleComponentRotation", true)
+addEventHandler(
+    "server_vehicle_components:setVehicleComponentRotation", root,
+        syncSetVehicleComponentRotation
+)
+
+-- sync functions
+function sendReadyMessage()
+    triggerServerEvent("server_vehicle_components:requestData", resourceRoot)
+end
+addEventHandler("onClientResourceStart", resourceRoot, sendReadyMessage)
+
+function receiveData(vehicles)
+    for vehicle, vehicle_data in pairs(vehicles) do
+        if isElementStreamedIn(vehicle) then
+            local visibles = vehicle_data.visibles
+            for component, visible in pairs(visibles) do
+                setVehicleComponentVisible(vehicle, component, visible)
+            end
+            local positions = vehicle_data.positions
+            for component, offsets in pairs(positions) do
+                setVehicleComponentPosition(vehicle, component, unpack(offsets))
+            end
+            local rotations = vehicle_data.rotations
+            for component, angles in pairs(rotations) do
+                setVehicleComponentRotation(vehicle, component, unpack(angles))
+            end
+        else
+            addLatentVehicle(vehicle, vehicle_data)
+        end
+    end
+end
+addEvent("server_vehicle_components:sendData", true)
+addEventHandler("server_vehicle_components:sendData", resourceRoot, receiveData)
+
+function syncLatentVehicles()
+    removeEventHandler("onClientElementStreamIn", source, syncLatentVehicles)
+
+    local vehicle_data = latent_vehicles[source]
+    if not vehicle_data then return end
+
+    local visibles = vehicle_data.visibles
+    if visibles then
+        for component, visible in pairs(visibles) do
+            setVehicleComponentVisible(source, component, visible)
+        end
+    end
+    local positions = vehicle_data.positions
+    if positions then
+        for component, offsets in pairs(positions) do
+            setVehicleComponentPosition(source, component, unpack(offsets))
+        end
+    end
+    local rotations = vehicle_data.rotations
+    if rotations then
+        for component, angles in pairs(rotations) do
+            setVehicleComponentRotation(source, component, unpack(angles))
+        end
+    end
+
+    latent_vehicles[source] = nil
+end
---
+++ server_vehicle_components/server_vehicle_components_s.lua
@@ -0,0 +1,118 @@
+-- main functions
+vehicles = {}
+-- vehicles[<vehicle>] = {
+--     visibles = {<component> = bool},
+--     positions = {<component> = {x, y, z, base}},
+--     rotations = {<rotation> = {rx, ry, rz, base}}
+-- }
+
+function setVehicleComponentVisible(vehicle, component, visible)
+    if not (isElement(vehicle) and vehicle.type == "vehicle") then return false end
+    if not vehicles[vehicle] then vehicles[vehicle] = {} end
+    if not vehicles[vehicle].visibles then vehicles[vehicle].visibles = {} end
+    vehicles[vehicle].visibles[component] = visible
+    triggerClientEvent(
+        "server_vehicle_components:setVehicleComponentVisible", vehicle, component,
+            visible
+    )
+end
+
+-- should be isVehicleComponentVisible, but was named same as client-side equivalent
+function getVehicleComponentVisible(vehicle, component)
+    if not (isElement(vehicle) and vehicle.type == "vehicle") then return nil end
+    local vehicle_data = vehicles[vehicle]
+    if not (vehicle_data and vehicle_data.visibles) then return nil end
+    return vehicle_data.visibles[component]
+end
+
+function setVehicleComponentPosition(vehicle, component, x, y, z, base)
+    if not (isElement(vehicle) and vehicle.type == "vehicle") then return false end
+    if base == nil then base = "root" end
+    if not (base == "parent" or base == "root" or base == "world") then return false end
+    if not vehicles[vehicle] then vehicles[vehicle] = {} end
+    if not vehicles[vehicle].positions then vehicles[vehicle].positions = {} end
+    vehicles[vehicle].positions[component] = {x, y, z, base}
+    triggerClientEvent(
+        "server_vehicle_components:setVehicleComponentPosition", vehicle, component, x,
+            y, z, base
+    )
+end
+
+function getVehicleComponentPosition(vehicle, component)
+    if not (isElement(vehicle) and vehicle.type == "vehicle") then return false end
+    local vehicle_data = vehicles[vehicle]
+    if not (vehicle_data and vehicle_data.positions) then return nil end
+    local offsets = vehicle_data.positions[component]
+    if not offsets then return nil end
+    return unpack(offsets)
+end
+
+function setVehicleComponentRotation(vehicle, component, rx, ry, rz, base)
+    if not (isElement(vehicle) and vehicle.type == "vehicle") then return false end
+    if base == nil then base = "root" end
+    if not (base == "parent" or base == "root" or base == "world") then return false end
+    if not vehicles[vehicle] then vehicles[vehicle] = {} end
+    if not vehicles[vehicle].rotations then vehicles[vehicle].rotations = {} end
+    vehicles[vehicle].rotations[component] = {rx, ry, rz, base}
+    triggerClientEvent(
+        "server_vehicle_components:setVehicleComponentRotation", vehicle, component, rx,
+            ry, rz, base
+    )
+end
+
+function getVehicleComponentRotation(vehicle, component)
+    if not (isElement(vehicle) and vehicle.type == "vehicle") then return false end
+    local vehicle_data = vehicles[vehicle]
+    if not (vehicle_data and vehicle_data.rotations) then return nil end
+    local offsets = vehicle_data.rotations[component]
+    if not offsets then return nil end
+    return unpack(offsets)
+end
+
+-- sync functions
+clients_synced = {}
+
+function sendData()
+    if clients_synced[client] then return end
+    triggerClientEvent(
+        client, "server_vehicle_components:sendData", resourceRoot, vehicles
+    )
+    clients_synced[client] = true
+end
+addEvent("server_vehicle_components:requestData", true)
+addEventHandler("server_vehicle_components:requestData", root, sendData)
+
+function removeDataSentFlag()
+    if isElement(source) then clients_synced[source] = nil end
+end
+addEventHandler("onPlayerQuit", root, removeDataSentFlag)
+
+function forgetVehicle(vehicle)
+    vehicles[vehicle] = nil
+end
+
+function forgetVehicleOnModelChange()
+    if isElement(source) and source.type == "vehicle" then forgetVehicle(source) end
+end
+addEventHandler("onElementModelChange", root, forgetVehicle)
+
+function forgetNonExistingVehicles()
+    local checkedcount = 0
+    while true do
+        for vehicle, _ in pairs(vehicles) do
+            if not isElement(vehicle) then forgetVehicle(vehicle) end
+            checkedcount = checkedcount + 1
+            if checkedcount >= 1000 then
+                coroutine.yield()
+                checkedcount = 0
+            end
+        end
+        coroutine.yield()
+    end
+end
+clearing_nonexisting_vehicless = coroutine.create(forgetNonExistingVehicles)
+setTimer(
+    function()
+        coroutine.resume(clearing_nonexisting_vehicless)
+    end, 2500, 0
+)
