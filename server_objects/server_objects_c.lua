b90d7ebf594a47203b74e5dfa63fd6f3f09ba324
Yamsha <jakub.lacinski@gmail.com>
2020-08-11 12:57:26 +0200

    Rework server_objects to newer standard - mirrors server_vehicle_components resource

--- server_objects/server_objects_c.lua
+++ server_objects/server_objects_c.lua
@@ -1,25 +1,95 @@
 -- main functions
-local function syncSetObjectBreakable(enabled)
-    setObjectBreakable(source, enabled)
+latent_objects = {}
+-- latent_objects[<object>] = {
+--     breakable = bool,
+--     mass = float
+-- }
+
+function addLatentObject(object, data)
+    print("latent object!")
+    latent_objects[object] = data or {}
+    addEventHandler("onClientElementStreamIn", object, syncLatentObject)
+end
+
+function syncSetObjectBreakable(breakable)
+    if isElementStreamedIn(source) then
+        setObjectBreakable(source, breakable)
+    else
+        if not latent_objects[source] then addLatentObject(source) end
+        latent_objects[source].breakable = breakable
+    end
 end
 addEvent("server_objects:setObjectBreakable", true)
 addEventHandler("server_objects:setObjectBreakable", root, syncSetObjectBreakable)
 
-local function syncSetObjectMass(value)
-    setObjectMass(source, value)
+function syncSetObjectMass(mass)
+    if isElementStreamedIn(source) then
+        setObjectMass(source, mass)
+    else
+        if not latent_objects[source] then addLatentObject(source) end
+        latent_objects[source].mass = mass
+    end
 end
 addEvent("server_objects:setObjectMass", true)
 addEventHandler("server_objects:setObjectMass", root, syncSetObjectMass)
 
 -- sync functions
-local function sendReadyMessage()
+function sendReadyMessage()
     triggerServerEvent("server_objects:requestData", resourceRoot)
 end
 addEventHandler("onClientResourceStart", resourceRoot, sendReadyMessage)
 
-local function receiveData(breakable, new_mass)
-    for object, enabled in pairs(breakable) do setObjectBreakable(object, enabled) end
-    for object, value in pairs(new_mass) do setObjectMass(object, value) end
+function receiveData(objects)
+    for object, object_data in pairs(objects) do
+        if isElementStreamedIn(object) then
+            local breakable = object_data.breakable
+            if breakable then setObjectBreakable(object, breakable) end
+            local mass = object_data.mass
+            if mass then setObjectMass(object, mass) end
+        else
+            addLatentObject(object, object_data)
+        end
+    end
 end
 addEvent("server_objects:sendData", true)
 addEventHandler("server_objects:sendData", resourceRoot, receiveData)
+
+function syncLatentObject()
+    print("syncing latent object!")
+    removeEventHandler("onClientElementStreamIn", source, syncLatentObject)
+
+    local object_data = latent_objects[source]
+    if not object_data then return end
+
+    local breakable = object_data.breakable
+    if breakable ~= nil then setObjectBreakable(source, breakable) end
+    local mass = object_data.mass
+    if mass then setObjectMass(source, mass) end
+
+    forgetObject(source)
+end
+
+function forgetObject(object)
+    latent_objects[object] = nil
+end
+
+function forgetNonExistingObjects()
+    local checkedcount = 0
+    while true do
+        for object, _ in pairs(latent_objects) do
+            if not isElement(object) then forgetObject(object) end
+            checkedcount = checkedcount + 1
+            if checkedcount >= 1000 then
+                coroutine.yield()
+                checkedcount = 0
+            end
+        end
+        coroutine.yield()
+    end
+end
+clearing_nonexisting_objects = coroutine.create(forgetNonExistingObjects)
+setTimer(
+    function()
+        coroutine.resume(clearing_nonexisting_objects)
+    end, 2500, 0
+)
--- server_objects/server_objects_s.lua
+++ server_objects/server_objects_s.lua
@@ -1,41 +1,90 @@
 -- main functions
-local breakable = {}
-local new_mass = {}
+objects = {}
+-- objects[<object>] = {
+--     breakable = bool,
+--     mass = float
+-- }
 
-function setObjectBreakable(object, enabled)
-    if getElementType(object) ~= "object" then return false end
-    breakable[object] = enabled
-    triggerClientEvent("server_objects:setObjectBreakable", object, enabled)
+function setObjectBreakable(object, breakable)
+    if not (isElement(object) and getElementType(object) == "object") then
+        return false
+    end
+    if type(breakable) ~= "boolean" then return false end
+
+    if not objects[object] then objects[object] = {} end
+    objects[object].breakable = breakable
+    triggerClientEvent("server_objects:setObjectBreakable", object, breakable)
     return true
 end
 
-function setObjectMass(object, value)
-    if getElementType(object) ~= "object" then return false end
-    new_mass[object] = value
-    triggerClientEvent("server_objects:setObjectMass", object, value)
+function isObjectBreakable(object)
+    if not (isElement(object) and getElementType(object) == "object") then
+        return false
+    end
+    local object_data = objects[object]
+    if not object_data then return nil end
+    return object_data.breakable
+end
+
+function setObjectMass(object, mass)
+    if not (isElement(object) and getElementType(object) == "object") then
+        return false
+    end
+    if not (tonumber(mass) and tonumber(mass) == mass) then return false end
+    if mass < 0 then return false end
+
+    if not objects[object] then objects[object] = {} end
+    objects[object].mass = mass
+    triggerClientEvent("server_objects:setObjectMass", object, mass)
     return true
+end
+
+function getObjectMass(object)
+    if not (isElement(object) and getElementType(object) == "object") then
+        return false
+    end
+    local object_data = objects[object]
+    if not object_data then return nil end
+    return object_data.mass
 end
 
 -- sync functions
-local clients_synced = {}
+clients_synced = {}
 
-local function sendData()
+function sendData()
     if clients_synced[client] then return end
-    triggerClientEvent(client, "server_objects:sendData", root, breakable, new_mass)
+    triggerClientEvent(client, "server_objects:sendData", root, objects)
     clients_synced[client] = true
 end
 addEvent("server_objects:requestData", true)
 addEventHandler("server_objects:requestData", root, sendData)
 
-local function removeDataSentFlag()
+function removeDataSentFlag()
     if isElement(source) then clients_synced[source] = nil end
 end
 addEventHandler("onPlayerQuit", root, removeDataSentFlag)
 
-local function forgetDestroyedObject()
-    if isElement(source) and getElementType(source) == "object" then
-        breakable[source] = nil
-        new_mass[source] = nil
+function forgetObject(object)
+    objects[object] = nil
+end
+
+function forgetNonExistingObjects()
+    local checkedcount = 0
+    while true do
+        for object, _ in pairs(objects) do
+            if not isElement(object) then forgetObject(object) end
+            checkedcount = checkedcount + 1
+            if checkedcount >= 1000 then
+                coroutine.yield()
+                checkedcount = 0
+            end
+        end
+        coroutine.yield()
     end
 end
-addEventHandler("onElementDestroy", root, forgetDestroyedObject)
+clearing_nonexisting_objects = coroutine.create(forgetNonExistingObjects)
+setTimer(
+    function()
+        coroutine.resume(clearing_nonexisting_objects)
+    end, 2500, 0
+)
